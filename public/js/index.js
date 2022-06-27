eaab825b8cf49cf8bb4c71a45216c7fb731bc3e1
HarshS2108 <harshs2108@outlook.com>
2022-06-27 15:29:34 +0530

    added custom/broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+let socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to server.');
+});
+socket.on('disconnect', () => {
+    console.log('Disconnected from server.');
+});
+socket.on('newMessage', function(message){
+    console.log('newMessage',message);
+});
